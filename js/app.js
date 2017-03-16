a1c477369b896636b2eb51b7d9668fd974347426
Jesse R Weigel <jesse@jesseweigel.com>
2017-03-16 15:21:09 -0400

    Add down arrow to hero section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(function() {
-    $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav();
+
+  //Smooth in page navigation
+  function scrollToAnchor(name){
+      var aTag = $("div[name='"+ name +"']");
+      $('html,body').animate({scrollTop: aTag.offset().top}, 'slow');
+  }
+
+  $(".slow-nav").click(function() {
+    event.preventDefault();
+    var name = $(this).attr('href');
+    scrollToAnchor(name);
+  });
 });
