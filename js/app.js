41a63420d648844087963093140fb9f9467fddb5
Jesse R Weigel <jesse@jesseweigel.com>
2017-03-20 15:50:28 -0400

    Detect if browser is mobile safari and change hero images styles accordingly.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ $(function() {
     closeOnClick: true
   });
 
+  //Fix for background-attachment: fixed issue on mobile Safari
+  //http://stackoverflow.com/questions/3007480/determine-if-user-navigated-from-mobile-safari
+  var ua = window.navigator.userAgent;
+  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  var webkit = !!ua.match(/WebKit/i);
+  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
+  if (iOS && webkit && iOSSafari) {
+    $('#home').attr('style', 'background-attachment: scroll; background-position-x: center;')
+  }
+
   //Smooth in page navigation
   function scrollToAnchor(name){
       var aTag = $("div[name='"+ name +"']");
